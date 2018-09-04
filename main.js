bdf20a7671c4612c10dde24cd91befb4b88f3c3e
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-09-04 11:24:14 +0800

    Displays seeded notes and event listener to delete note

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert("setup ok!");
+const todo = document.querySelector("#note-list");
+
+// seed data
+const notes = ["note1", "note2", "note3"];
+
+const createNote = note => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+
+  li.textContent = note;
+  todo.appendChild(li);
+  span.textContent = "    X";
+  li.appendChild(span);
+
+  span.addEventListener("click", () => {
+    li.style.display = "none";
+  });
+};
+
+notes.forEach(note => createNote(note));
